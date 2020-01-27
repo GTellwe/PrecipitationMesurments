da5c94fee97ddece9acf8e7c85aca794bdf13996
gustav <g.tellwe@gmail.com>
2020-01-27 12:29:30 +0100

    added download function

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import numpy as np
 
 '''
     functions:
-        getGOESDataMatrix()
+        getFileForHour():
+        getClosestFile():
+        downloadFile():
     
 '''
 
   
 
-#%%
+#%% Functions
 def getFilesForHour(DATE):
     
     '''
@@ -31,20 +33,26 @@ def getFilesForHour(DATE):
 
 def getClosestFile(DATE):
     import datetime
+    import numpy as np
     '''
     Returns the filepath closest to the DATE object
     
     '''
     files_for_hour = list(map(lambda x : x.name, getFilesForHour(DATE)))
     date_diff = map(lambda x: abs(datetime.strptime(x.split('_')[3], 's%Y%j%H%M%S%f')-DATE), files_for_hour)
-    return 'ABI-L1b-RadC/%s' % (files_for_hour[np.argmin(date_diff)]) 
+    return '%s' % (files_for_hour[np.argmin(date_diff)]) 
 
 def downloadFile(FILE):
+    '''
+    Downloads FILE and saves it in the data folder
+    '''
     from google.cloud import storage
     client = storage.Client.create_anonymous_client()
     bucket = client.bucket(bucket_name='gcp-public-data-goes-16', user_project=None)
     blob = bucket.blob(FILE)
-    blob.download_to_filename('data/test.nc')
+    blob.download_to_filename('data/'+FILE.split('/')[-1])
     
 #%%
+import datetime
+print(getClosestFile(datetime.datetime(2020,1,26)))
 downloadFile(getClosestFile(datetime.datetime(2020,1,26)))
