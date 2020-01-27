1416dcc2ac97547d503d76f576cc451b5c141db7
gustav <g.tellwe@gmail.com>
2020-01-27 11:41:27 +0100

    added functions for retriving the file name for the closest file in google cloud storage

--- main.py
+++ main.py
@@ -2,37 +2,49 @@
 """
 Created on Wed Jan 22 12:19:27 2020
 
-@author: gustav
+@author: Gustav Tellwe
 """
-
-import h5py
-filename = '2B.GPM.DPRGMI.CORRA2018.20200119-S234145-E011417.033483.V06A.SUB.hdf5'
-
-with h5py.File(filename, 'r') as f:
-    # List all groups
-    print("Keys: %s" % f.keys())
-    a_group_key = list(f.keys())[0]
-
-    # Get the data
-    data = list(f[a_group_key])
-    
-    
-#%%
-    # Import the necessary packages and modules
-import matplotlib.pyplot as plt
-import numpy as np
-
-import h5py
-filename = '2B.GPM.DPRGMI.CORRA2018.20200119-S234145-E011417.033483.V06A.SUB.hdf5'
-f = h5py.File(filename, 'r')
-print(f.keys())
-print(f['MS'].keys())
-print(f['nrayMS_idx'][:])
-data1 = f['MS']['surfPrecipTotRate']
-print(data1.shape)
-plt.plot(data1[100:1200,15])
-
-#%%
 import netCDF4
 import numpy as np
-f = netCDF4.Dataset('M6C01_G16_s20200240000163_e20200240009471_c20200240009528.nc')
+
+'''
+    functions:
+        getGOESDataMatrix()
+    
+'''
+
+  
+
+#%%
+def getFilesForHour(DATE):
+    
+    '''
+        Returns an iterator of file names for the specific hour defines by DATE
+    '''
+    from google.cloud import storage
+    
+    client = storage.Client.create_anonymous_client()
+    bucket = client.bucket(bucket_name='gcp-public-data-goes-16', user_project=None)
+    PATH_Storage = 'ABI-L1b-RadC/%s' % (DATE.strftime('%Y/%j/%H')) 
+    
+    return bucket.list_blobs(prefix = PATH_Storage)
+
+def getClosestFile(DATE):
+    import datetime
+    '''
+    Returns the filepath closest to the DATE object
+    
+    '''
+    files_for_hour = list(map(lambda x : x.name, getFilesForHour(DATE)))
+    date_diff = map(lambda x: abs(datetime.strptime(x.split('_')[3], 's%Y%j%H%M%S%f')-DATE), files_for_hour)
+    return 'ABI-L1b-RadC/%s' % (files_for_hour[np.argmin(date_diff)]) 
+
+def downloadFile(FILE):
+    from google.cloud import storage
+    client = storage.Client.create_anonymous_client()
+    bucket = client.bucket(bucket_name='gcp-public-data-goes-16', user_project=None)
+    blob = bucket.blob(FILE)
+    blob.download_to_filename('data/test.nc')
+    
+#%%
+downloadFile(getClosestFile(datetime.datetime(2020,1,26)))
